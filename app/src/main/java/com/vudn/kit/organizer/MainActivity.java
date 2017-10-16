0b546adf092c6f977e7c23a80b2010ba02b397df
kam.tayupov <k.i.tayupov@mail.ru>
2017-10-16 09:19:26 +0300

    init note list controls in main activity

--- app/src/main/java/com/vudn/kit/organizer/MainActivity.java
+++ app/src/main/java/com/vudn/kit/organizer/MainActivity.java
@@ -5,11 +5,21 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+
+import com.vudn.kit.organizer.note.Note;
+import com.vudn.kit.organizer.note.NoteAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private ArrayList<Note> arrayList;
+    private NoteAdapter noteAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +36,15 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        initControls();
+    }
+
+    private void initControls() {
+        arrayList = new ArrayList<>();
+        noteAdapter = new NoteAdapter(arrayList);
+        listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(noteAdapter);
     }
 
     @Override
