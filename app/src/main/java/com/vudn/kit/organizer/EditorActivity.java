f05ba1d5298dd04d0118b4c375f6c319c318f8ad
kam.tayupov <k.i.tayupov@mail.ru>
2017-10-16 10:25:34 +0300

    create floating action button for editor activity

--- app/src/main/java/com/vudn/kit/organizer/EditorActivity.java
+++ app/src/main/java/com/vudn/kit/organizer/EditorActivity.java
@@ -1,13 +1,25 @@
 package com.vudn.kit.organizer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class EditorActivity extends AppCompatActivity {
+public class EditorActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+
+        findViewById(R.id.fab).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.fab:
+                finish();
+                break;
+        }
     }
 }
