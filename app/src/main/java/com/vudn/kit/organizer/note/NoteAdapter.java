3520016f9fed8bcfef40c076644e7006a958cc78
kam.tayupov <k.i.tayupov@mail.ru>
2017-10-16 09:06:41 +0300

    create list view adapter

---
+++ app/src/main/java/com/vudn/kit/organizer/note/NoteAdapter.java
@@ -0,0 +1,49 @@
+package com.vudn.kit.organizer.note;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.vudn.kit.organizer.R;
+
+import java.util.ArrayList;
+
+public class NoteAdapter extends BaseAdapter {
+
+    private ArrayList<Note> arrayList;
+
+    public NoteAdapter(ArrayList<Note> arrayList) {
+        this.arrayList = arrayList;
+    }
+
+    @Override
+    public int getCount() {
+        return arrayList.size();
+    }
+
+    @Override
+    public Note getItem(int position) {
+        return arrayList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout, null);
+        }
+        return noteView(convertView, position);
+    }
+
+    private View noteView(View view, int position) {
+        final Note item = getItem(position);
+        ((TextView) view.findViewById(R.id.textView)).setText(item.getBody());
+        return view;
+    }
+}
