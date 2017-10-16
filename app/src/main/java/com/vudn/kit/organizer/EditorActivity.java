38fdca14f8c0cb2dac74e0a197c08d428ebfb75b
kam.tayupov <k.i.tayupov@mail.ru>
2017-10-16 10:39:06 +0300

    send data to main activity

--- app/src/main/java/com/vudn/kit/organizer/EditorActivity.java
+++ app/src/main/java/com/vudn/kit/organizer/EditorActivity.java
@@ -1,15 +1,23 @@
 package com.vudn.kit.organizer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.vudn.kit.organizer.note.NoteDBHelper;
+
 public class EditorActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextInputEditText bodyEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+
+        bodyEditText = (TextInputEditText) findViewById(R.id.textInputEditText);
 
         findViewById(R.id.fab).setOnClickListener(this);
     }
@@ -18,8 +26,15 @@ public class EditorActivity extends AppCompatActivity implements View.OnClickLis
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.fab:
-                finish();
+                sendResult();
                 break;
         }
+    }
+
+    private void sendResult() {
+        final Intent intent = new Intent();
+        intent.putExtra(NoteDBHelper.BODY, bodyEditText.getText().toString());
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
