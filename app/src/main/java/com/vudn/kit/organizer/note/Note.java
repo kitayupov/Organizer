b7132fc51b036f79a72a35046256ab8b9db6676b
kam.tayupov <k.i.tayupov@mail.ru>
2017-10-16 10:42:23 +0300

    implement parcelable interface for Note

--- app/src/main/java/com/vudn/kit/organizer/note/Note.java
+++ app/src/main/java/com/vudn/kit/organizer/note/Note.java
@@ -1,6 +1,9 @@
 package com.vudn.kit.organizer.note;
 
-public class Note {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Note implements Parcelable {
 
     private String body;
 
@@ -11,6 +14,22 @@ public class Note {
     public Note(String body) {
         this.body = body;
     }
+
+    private Note(Parcel in) {
+        body = in.readString();
+    }
+
+    public static final Creator<Note> CREATOR = new Creator<Note>() {
+        @Override
+        public Note createFromParcel(Parcel in) {
+            return new Note(in);
+        }
+
+        @Override
+        public Note[] newArray(int size) {
+            return new Note[size];
+        }
+    };
 
     public String getBody() {
         return body;
@@ -25,5 +44,15 @@ public class Note {
         return "Note{" +
                 "body='" + body + '\'' +
                 '}';
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(body);
     }
 }
