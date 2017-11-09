cc1836cf30afea3998f2b89e60001d0b3ef3bbd5
kam.tayupov <k.i.tayupov@mail.ru>
2017-11-09 14:18:27 +0300

    create target date field for database

--- app/src/main/java/com/vudn/kit/organizer/note/NoteDBHelper.java
+++ app/src/main/java/com/vudn/kit/organizer/note/NoteDBHelper.java
@@ -12,15 +12,16 @@ public class NoteDBHelper extends SQLiteOpenHelper {
     public static final String TABLE_NAME = "Notes";
     public static final String NAME = "name";
     public static final String BODY = "body";
+    public static final String DATE_TARGET = "dateTarget";
     public static final String TIME_CREATED = "timeCreated";
     public static final String TIME_UPDATED = "timeUpdated";
     public static final String COMPLETED = "completed";
 
-    public static final String WHERE_CLAUSE = NAME + "=? and " + BODY + "=? and " + TIME_CREATED + "=? and "
-            + TIME_UPDATED + "=? and " + COMPLETED + "=?";
+    public static final String WHERE_CLAUSE = NAME + "=? and " + BODY + "=? and " + DATE_TARGET + "=? and "
+            + TIME_CREATED + "=? and " + TIME_UPDATED + "=? and " + COMPLETED + "=?";
 
     private static final String NOTES_DB = "notes.db";
-    private static final int VERSION = 4;
+    private static final int VERSION = 5;
 
     public NoteDBHelper(Context context) {
         this(context, NOTES_DB, null, VERSION);
@@ -67,6 +68,13 @@ public class NoteDBHelper extends SQLiteOpenHelper {
                 );
                 onUpgrade(db, oldVersion + 1, newVersion);
                 break;
+            case 4:
+                db.execSQL(
+                        String.format(
+                                "alter table %s add column %s text default %d",
+                                TABLE_NAME, DATE_TARGET, -1)
+                );
+                break;
             default:
         }
     }
@@ -76,7 +84,7 @@ public class NoteDBHelper extends SQLiteOpenHelper {
                 String.format(
                         "create table %s (%s integer primary key autoincrement, " +
                                 "%s text, %s text, %s numeric, %s numeric, %s numeric)",
-                        TABLE_NAME, _ID, NAME, BODY, TIME_CREATED, TIME_UPDATED, COMPLETED
+                        TABLE_NAME, _ID, NAME, BODY, DATE_TARGET, TIME_CREATED, TIME_UPDATED, COMPLETED
                 );
     }
 }
