81318dfb8996bcfad687166228299f7f513efab6
kam.tayupov <k.i.tayupov@mail.ru>
2017-11-10 17:38:33 +0300

    implement date selection

--- app/src/main/java/com/vudn/kit/organizer/fragment/DateDialogFragment.java
+++ app/src/main/java/com/vudn/kit/organizer/fragment/DateDialogFragment.java
@@ -3,6 +3,7 @@ package com.vudn.kit.organizer.fragment;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CalendarView;
@@ -13,22 +14,36 @@ import com.vudn.kit.organizer.note.NoteDBHelper;
 
 public class DateDialogFragment extends DialogFragment {
 
+    private CalendarView calendarView;
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         return new AlertDialog.Builder(getActivity())
                 .setView(getContentView())
                 .setNegativeButton("No", null)
-                .setPositiveButton("Yes", null)
+                .setPositiveButton("Yes", clickListener)
                 .create();
     }
 
     private View getContentView() {
         final View view = getActivity().getLayoutInflater().inflate(R.layout.dialog_date, null);
-        long dateTarget = getArguments().getLong(NoteDBHelper.DATE_TARGET);
-        if (dateTarget != Note.DEFAULT_DATE_TARGET) {
-            final CalendarView calendarView = view.findViewById(R.id.calendarView);
-            calendarView.setDate(dateTarget);
-        }
+        calendarView = view.findViewById(R.id.calendarView);
+        setDateTarget();
         return view;
     }
+
+    private void setDateTarget() {
+        final long dateTarget = getArguments().getLong(NoteDBHelper.DATE_TARGET);
+        if (dateTarget != Note.DEFAULT_DATE_TARGET) {
+            calendarView.setDate(dateTarget);
+        }
+    }
+
+    private DialogInterface.OnClickListener clickListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            final long selectedDate = calendarView.getDate();
+            System.out.println(selectedDate);
+        }
+    };
 }
